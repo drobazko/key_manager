5e954ea23c020148a2ba77cb940663896747fece
Alex Drobazko <drobazko@gmail.com>
2014-06-20 10:22:04 +0300

    first commit

---
+++ app/controllers/credentials_controller.rb
@@ -0,0 +1,19 @@
+class CredentialsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
